a34807da7e3e14103de1a3880236492cc068a372
YN Chen <yn.chen.mtk@gmail.com>
2021-04-05 21:50:03 +0800

    fix kernel crash w/o FW
    commit ea2f7570081e6da1d511f582cbadbae28713ff37
    Author: Sean Wang <objelf@gmail.com>
    Date:   Fri Mar 12 08:03:18 2021 -0800
    
        FROMGIT: mt76: mt7921: fix kernel crash when the firmware fails to download

--- drivers/net/wireless/mediatek/mt76/mt7921/pci.c
+++ drivers/net/wireless/mediatek/mt76/mt7921/pci.c
@@ -145,18 +145,18 @@ static int mt7921_pci_probe(struct pci_dev *pdev,
 	ret = devm_request_irq(mdev->dev, pdev->irq, mt7921_irq_handler,
 			       IRQF_SHARED, KBUILD_MODNAME, dev);
 	if (ret)
-		goto err_free_irq;
+		goto err_free_dev;
 
 	ret = mt7921_register_device(dev);
 	if (ret)
-		goto err_free_dev;
+		goto err_free_irq;
 
 	return 0;
 
+err_free_irq:
+	devm_free_irq(&pdev->dev, pdev->irq, dev);
 err_free_dev:
 	mt76_free_device(&dev->mt76);
-err_free_irq:
-	devm_free_irq(mdev->dev, pdev->irq, dev);
 err_free_pci_vec:
 	pci_free_irq_vectors(pdev);
 
